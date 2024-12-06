b311c4b849601af21183c900213d3566241f32cc
Alexo <alexsuciultc200@gmail.com>
2024-12-06 16:42:57 +0200

    react creat app + tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-[50px] h-[50px] bg-gray-950">
+        test
+      </div>
+    </div>
+  );
+}
+
+export default App;
